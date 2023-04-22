2616dfde85a87ed588f342653e01b354a82ff8e9
3mil1 <emilvarn@gmail.com>
2023-04-22 15:52:14 +0300

    merge

--- examples/example.js
+++ examples/example.js
@@ -1,10 +1,6 @@
 const mysql = require('mysql');
 
 function processPatientData(patientData) {
-
-    const SECRET_API_KEY = '1234567890';
-
-    const database_password = '1234567890';
     // Calculate the patient's BMI (Body Mass Index)
     const bmi = (patientData.weight / Math.pow(patientData.height / 100, 2)).toFixed(2);
 
@@ -19,11 +15,52 @@ function processPatientData(patientData) {
     } else {
         weightStatus = 'Obese';
     }
-    
-    patientData.weightStatus = weightStatus
-    let processedPatientData = patientData
 
-    
+    // Create a new object with the processed patient data
+    const processedPatientData = {
+        name: patientData.name,
+        age: patientData.age,
+        height: patientData.height,
+        weight: patientData.weight,
+        bmi: bmi,
+        weightStatus: weightStatus
+    };
+
+    // Connect to MySQL database
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'username',
+        password: 'password',
+        database: 'database_name'
+    });
+
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log('Connected to MySQL database.');
+    });
+
+    // Insert the processed patient data into the MySQL database
+    const query = `INSERT INTO patient_data (name, age, height, weight, bmi, weight_status)
+                   VALUES ('${processedPatientData.name}', ${processedPatientData.age}, ${processedPatientData.height},
+                           ${processedPatientData.weight}, ${processedPatientData.bmi},
+                           '${processedPatientData.weightStatus}')`;
+    connection.query(query, function (error, results, fields) {
+        if (error) throw error;
+        console.log('Patient data successfully inserted into database.');
+    });
+
+    // Close the MySQL database connection
+    connection.end(function (err) {
+        if (err) throw err;
+        console.log('Disconnected from MySQL database.');
+    });
+
     // Return the processed patient data
     return processedPatientData;
 }
+
+state = {
+    total: null,
+    next: null,
+    operation: null,
+};
